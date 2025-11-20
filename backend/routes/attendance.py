c5c098aae4bc1a6a2b6917248597c9545231561e
unknown <qkdrk7777775@gmail.com>
2025-11-20 14:07:34 +0900

    feat: 출석 기능[FR-33] 백엔드 구현

--- backend/routes/attendance.py
+++ backend/routes/attendance.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 from controllers import attendance
 
 router = APIRouter()
-router.add_api_route("/{post_id}",attendance.create_comment, methods=["POST"])
-router.add_api_route("/{post_id}",attendance.get_comment_by_post_id, methods=["GET"])
+router.add_api_route("/{post_id}",attendance.create_attendance, methods=["POST"])
+router.add_api_route("/{post_id}",attendance.get_attendance_by_user_id, methods=["GET"])
